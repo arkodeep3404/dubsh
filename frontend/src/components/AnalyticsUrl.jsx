af9687e3d25113a52b80eba3bc7b393762633bf9
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-05-09 23:31:54 +0530

    fixed sp

--- frontend/src/components/AnalyticsUrl.jsx
+++ frontend/src/components/AnalyticsUrl.jsx
@@ -102,16 +102,16 @@ export default function Analytics() {
       <div className="w-auto m-3 peer h-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
         Contact Details :
       </div>
-      {ContactDetails.map((contact) => (
+      {ContactDetails.map((contacts) => (
         <div className="flex">
           <div className="w-auto m-3 peer h-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
-            Name : {contact.name}
+            Name : {contacts.name}
           </div>
           <div className="w-auto m-3 peer h-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
-            Email : {contact.email}
+            Email : {contacts.email}
           </div>
           <div className="w-auto m-3 peer h-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50">
-            Phone Number : {contact.phoneNumber}
+            Phone Number : {contacts.phoneNumber}
           </div>
         </div>
       ))}
